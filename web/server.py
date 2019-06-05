fdad8658945f66ce971c31b8d3818a231044d9f7
THEFLILUX <jorge.nicho@utec.edu.pe>
2019-06-05 13:04:07 -0500

    Login con iconos

--- web/server.py
+++ web/server.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template, request, session, Response, redirect
 from database import connector
 from model import entities
 import json
+import time
 
 db = connector.Manager()
 engine = db.createEngine()
@@ -16,6 +17,24 @@ def index():
 def static_content(content):
     return render_template(content)
 
+@app.route('/authenticate', methods = ['POST'])
+def authenticate():
+    time.sleep(4)
+    message = json.loads(request.data)
+    username = message['username']
+    password = message['password']
+    #look in database
+    db_session = db.getSession(engine)
+    try:
+        user = db_session.query(entities.User
+        ).filter(entities.User.username == username
+        ).filter(entities.User.password == password
+        ).one()
+        message = {'message': 'Authorized'}
+        return Response(message, status=200, mimetype='application/json')
+    except Exception:
+        message = {'message': 'Authorized'}
+        return Response(message, status=401, mimetype='application/json')
 
 @app.route('/users', methods = ['GET'])
 def get_users():
@@ -25,26 +44,6 @@ def get_users():
     for user in dbResponse:
         data.append(user)
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
-
-
-@app.route('/users/<id>', methods = ['GET'])
-def get_user(id):
-    db_session = db.getSession(engine)
-    users = db_session.query(entities.User).filter(entities.User.id == id)
-    for user in users:
-        js = json.dumps(user, cls=connector.AlchemyEncoder)
-        return  Response(js, status=200, mimetype='application/json')
-
-    message = { 'status': 404, 'message': 'Not Found'}
-    return Response(message, status=404, mimetype='application/json')
-
-@app.route('/create_test_users', methods = ['GET'])
-def create_test_users():
-    db_session = db.getSession(engine)
-    user = entities.User(name="David", fullname="Lazo", password="1234", username="qwerty")
-    db_session.add(user)
-    db_session.commit()
-    return "Test user created!"
 
 @app.route('/users', methods = ['POST'])
 def create_user():
@@ -73,14 +72,33 @@ def update_user():
     return 'Updated User'
 
 @app.route('/users', methods = ['DELETE'])
-def delete_message():
+def delete_user():
     id = request.form['key']
     session = db.getSession(engine)
-    messages = session.query(entities.User).filter(entities.User.id == id)
-    for message in messages:
-        session.delete(message)
+    users = session.query(entities.User).filter(entities.User.id == id)
+    for user in users:
+        session.delete(user)
     session.commit()
-    return "Deleted Message"
+    return "Deleted User"
+
+@app.route('/users/<id>', methods = ['GET'])
+def get_user(id):
+    db_session = db.getSession(engine)
+    users = db_session.query(entities.User).filter(entities.User.id == id)
+    for user in users:
+        js = json.dumps(user, cls=connector.AlchemyEncoder)
+        return  Response(js, status=200, mimetype='application/json')
+
+    message = { 'status': 404, 'message': 'Not Found'}
+    return Response(message, status=404, mimetype='application/json')
+
+@app.route('/create_test_users', methods = ['GET'])
+def create_test_users():
+    db_session = db.getSession(engine)
+    user = entities.User(name="David", fullname="Lazo", password="1234", username="qwerty")
+    db_session.add(user)
+    db_session.commit()
+    return "Test user created!"
 
 if __name__ == '__main__':
     app.secret_key = ".."
